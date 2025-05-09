82e0cef885bf2be797541b9f5f2a6bb32aa97a25
your_username <your_email>
2025-05-09 20:40:48 +0800

    http->https

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://dev.theimitation.ai:3000/";
-export const WS_URL = "ws://dev.theimitation.ai:3000/";
+export const BASE_URL = "https://dev.theimitation.ai:3000/";
+export const WS_URL = "wss://dev.theimitation.ai:3000/";
